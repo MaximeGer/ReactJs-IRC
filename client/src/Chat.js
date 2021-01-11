1489a6a2f2a86072dd8cb76e02946a6e33ff798d
Ghazkull11 <49782457+Ghazkull11@users.noreply.github.com>
2021-01-11 15:58:10 +0100

    all command added but no connection with bdd

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -8,6 +8,8 @@ class Chat extends React.Component{
         this.state = {
             username: '',
             message: '',
+            error: '',
+            channel: [],
             messages: []
         };
 
@@ -29,21 +31,99 @@ class Chat extends React.Component{
             var message = this.state.message;
             var nickRegex = new RegExp("^/nick");
             var listRegex = new RegExp("^/list");
+            var createRegex = new RegExp("^/create");
+            var deleteRegex = new RegExp("^/delete");
+            var joinRegex = new RegExp("^/join");
+            var quitRegex = new RegExp("^/quit");
+            var usersRegex = new RegExp("^/users");
+            var msgRegex = new RegExp("^/msg");
 
+            var commandString = "";
+            this.state.error = "";
 
             if(nickRegex.test(message)){
                 this.state.username = message.slice(6);
 
             } else if (listRegex.test(message)){
-                var listString = message.slice(6);
-                if ( listString == "" || listString == null){
-                    console.log("Display all channel available " + listString);
+                commandString = message.slice(6);
+                if ( commandString === "" || commandString === " " || commandString === null){
+                    // DISPLAY ALL CHANNEL
+                    console.log("Display all channel available " + commandString);
                     // TODO Display all channel available
                 } else {
-                    console.log("Display all channel available according to a string " + listString);
+                    // DISPLAY ALL CHANNEL OF A STRING
+                    console.log("Display all channel available according to the string : " + commandString);
                     // TODO Display all channel available according to a string
                 }
+            } else if (createRegex.test(message)){
+                commandString = message.slice(8);
+                if(commandString === "" || commandString === " " ||  commandString === null){
+                    this.state.error ="You have to specify a name for your new channel : \"/create newChannel\"";
+                } else if(false){
+                    // If command pas bon format : 
+                    this.state.error ="The name of your new channel have to be of the format : ";
+                } else {
+                    // CREATE CHANNEL 
+                    console.log("Create a channel with the name : " + commandString);
+                }
+            } else if (deleteRegex.test(message)){
+                commandString = message.slice(8);
+                if(commandString === "" || commandString === " " ||  commandString === null){
+                    this.state.error ="You have to specify a name for the channel you want to delete : \"/delete newChannel\"";
+                } else if(false){
+                    // If does not exist :
+                    this.state.error = "This channel has already been deleted or does not exist : ";
+                } else {
+                    // DELETE CHANNEL
+                    console.log("Delete the channel with the name : " + commandString + ", if it exist");
+                }
+
+            } else if (joinRegex.test(message)){
+                commandString = message.slice(6);
+                if(commandString === "" || commandString === " " ||  commandString === null){
+                    this.state.error ="You have to specify a name for the channel you want to join : \"/join newChannel\"";
+                } else if(false){
+                    // If does not exist :
+                    this.state.error = "This channel does not exist : ";
+                } else {
+                    // JOIN CHANNEL
+                    console.log("Join the channel with the name : " + commandString);
+                }
+
+            } else if (quitRegex.test(message)){
+                commandString = message.slice(6);
+                if(commandString === "" || commandString === " " ||  commandString === null){
+                    this.state.error ="You have to specify a name for the channel you want to quit : \"/quit newChannel\"";
+                } else if(false){
+                    // If does not exist :
+                    this.state.error = "This channel does not exist : ";
+                } else if(false){
+                    // If does not part of this channel :
+                    this.state.error = "You are not part of this channel : ";
+                } else {
+                    // QUIT CHANNEL
+                    console.log("Quit the channel with the name : " + commandString + " if it exist and you joined it");
+                }
+            } else if (usersRegex.test(message)){
+                commandString = message.slice(7);
+                // LIST ALL USERS
+                console.log("List users on the channel with the name : " + commandString);
+
+            } else if (msgRegex.test(message)){
+                commandString = message.slice(5);
+                if(commandString === "" || commandString === null){
+                    this.state.error ="You have to specify a name and a message to send  : \"/msg nickname message\"";
+                } else {
+
+                }
+                var nickToSend = new RegExp("^([^s]+)").exec(commandString);
+                var messageToSend = "commandString.slice(nickToSend)";
+
+                console.log("Send " + messageToSend + " to the user with the name : " + nickToSend);
+
             } else {
+                // NORMAL MESSAGE
+                // SAVE TO BDD - with author + message + channel + time? 
                 this.socket.emit('SEND_MESSAGE', {
                     author: this.state.username,
                     message: message
@@ -72,6 +152,7 @@ class Chat extends React.Component{
                             </div>
                             <div className="card-footer">
                                 <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
+                                <div className="errorCommands">{this.state.error}</div>
                                 <br/>
                                 <button onClick={this.sendMessage} className="btn btn-primary form-control">Send</button>
                             </div>
