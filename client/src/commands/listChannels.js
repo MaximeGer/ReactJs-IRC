6d36c6adf1e03ee298a39632baa53b32e1057be7
Ghazkull11 <49782457+Ghazkull11@users.noreply.github.com>
2021-01-25 09:49:14 +0100

    update /list

--- client/src/commands/listChannels.js
+++ client/src/commands/listChannels.js
@@ -1,12 +1,10 @@
-const listChannels = (name, Chat) => {
+const listChannels = async (name, Chat) => {
 
     await fetch("http://localhost:9000/api/channels/regex/" + name, {
         method: 'GET',
     }).then(response => {
         if (response.status === 200) {
             Chat.setState({ success: "The channel \"" + name + "\" has been successfully joined" });
-            channelExists = true;
-            id = response.id;
         } else if (response.status === 404) {
             Chat.setState({ error: "The channel \"" + name + "\" couldn't be found" });
         } else {
